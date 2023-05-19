262cccf40dd6eb42078d23146e73a321fb5f82df
asifhossain3131 <asifhossain3131@gmail.com>
2023-05-20 02:03:16 +0600

    banner info api added

--- index.js
+++ index.js
@@ -27,11 +27,27 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyToken=(req,res,next)=>{
+ const authorization=req.headers.authorization
+  if(!authorization){
+return res.status(401).send({error:true, message:'unauthorized access'})
+ }
+ const token=authorization.split(' ')[1]
+ jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
+  if(err){
+return res.status(401).send({error:true, message:'unauthorized access'})
+  }
+  req.decoded=decoded
+  next()
+ })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const toysCollections=client.db('intelliKids').collection('allToys')
+    const bannerCollections=client.db('intelliKids').collection('bannerInfo')
  
 
     // jwt 
@@ -46,17 +62,20 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/toys/:id',async(req,res)=>{
+    app.get('/toys/:id', async(req,res)=>{
       const id=req.params.id
       const query={_id: new ObjectId(id)}
       const result=await toysCollections.findOne(query)
       res.send(result)
     })
 
-    app.get('/mytoys/',async(req,res)=>{ 
+    app.get('/mytoys/', verifyToken, async(req,res)=>{ 
+      const decoded=req.decoded
       const email=req.query.email
+      if(decoded.email!==email){
+        return res.status(403).send({error:true, message:'Forbidden access'})
+      }
       const sort=req.query.sort
-      console.log(sort)
       let query={}
       if(email){
         query={sellerEmail:email}
@@ -101,6 +120,12 @@ async function run() {
       res.send(result)
     })
 
+
+    // banner info
+    app.get('/bannerInfo',async(req,res)=>{
+      const result=await bannerCollections.find().toArray()
+      res.send(result)
+    })
 
     
     // Send a ping to confirm a successful connection
